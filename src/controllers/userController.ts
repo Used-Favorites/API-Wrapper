26ba7f64ebf9ed9e505e5e3cf408b15bb0690074
Polabiel <bielgabrieloliveira77@gmail.com>
2024-06-06 22:52:20 -0300

    Add Prisma migrations and dev database files, ignore Swagger output JSON and assets

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from "express";
 
+const users: { name: string; email: string; password: string }[] = [];
+
 const userController = {
   create: async (req: Request, res: Response) => {
     const { name, email, password } = req.body;
+
+    if (users.find((user) => user.email === email)) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    users.push({
+      name,
+      email,
+      password,
+    });
 
     res.json({
       message: "User created",
