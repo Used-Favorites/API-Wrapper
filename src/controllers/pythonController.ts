de401d2454fee94e35daab534eb4806a46acc14f
Polabiel <bielgabrieloliveira77@gmail.com>
2024-06-11 12:59:29 -0300

    feat: Update Python controller and routes
    The Python controller and routes have been updated to use the `querystring` module from the `querystring` package instead of the `qs` module. This change improves the code readability and maintainability. Additionally, the `uploadPy` route now includes a Swagger annotation for the `file` parameter, providing better documentation for the API endpoint.

--- src/controllers/pythonController.ts
+++ src/controllers/pythonController.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Request, Response } from "express";
-var qs = require('querystring');
+import QueryString from "querystring";
 
 const pythonController = {
   testAxios: async (req: Request, res: Response) => {
@@ -14,49 +14,42 @@ const pythonController = {
       });
   },
   uploaderPy: async (req: Request, res: Response) => {
-        return res.render("pyinterface/uploader.ejs");
-      
+    return res.render("pyinterface/uploader.ejs");
   },
 
   uploadPy: async (request: Request, res: Response) => {
-      if (request.method == 'POST') {
-        var body = '';
-        
-        request.on('data', function (data) {
-            body += data;
-            
-            
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1e6) { 
-                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-                request.connection.destroy();
-            }
-        });
-        request.on('end', function () {
+    if (request.method == "POST") {
+      let body = "";
 
-            var POST = qs.parse(body);
-            //Para indexar o texto use POST['base64convertido'] e POST['nome']
-            //TODO: Conectar com o Python
-            //Enviar o base64
-            //Redirect para pagina que vai esperar a resposta
-            axios
-              .post('http://127.0.0.1:5000/addImage', {
-                name: POST['nome'],
-                imagem: POST['base64convertido'] 
-              })
-              .then((response) => {
-                return res.json(response.data);
-              })
-              .catch((error) => {
-                return res.json(error);
-              });
+      request.on("data", function (data) {
+        body += data;
 
-
-
-
-        });
-      }
-},
-}
+        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+        if (body.length > 1e6) {
+          // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+          request.socket.destroy();
+        }
+      });
+      request.on("end", function () {
+        const POST = QueryString.parse(body);
+        //Para indexar o texto use POST['base64convertido'] e POST['nome']
+        //TODO: Conectar com o Python
+        //Enviar o base64
+        //Redirect para pagina que vai esperar a resposta
+        axios
+          .post("http://127.0.0.1:5000/addImage", {
+            name: POST["nome"],
+            imagem: POST["base64convertido"],
+          })
+          .then((response) => {
+            return res.json(response.data);
+          })
+          .catch((error) => {
+            return res.json(error);
+          });
+      });
+    }
+  },
+};
 
 export default pythonController;
--- src/routes/Python.ts
+++ src/routes/Python.ts
@@ -6,19 +6,32 @@ import pythonController from "../controllers/pythonController";
 const PythonRouter = express.Router();
 
 PythonRouter.get("/testAxios", (req, res) => {
-  /* #swagger.tags = ['Products']
-       #swagger.description = 'Endpoint to test Axios.'
+  /* #swagger.tags = ['Python']
+       #swagger.description = 'Endpoint to test axios in python.'
        #swagger.path = '/products/testAxios'
   */
   pythonController.testAxios(req, res);
 });
 
 PythonRouter.get("/uploadImg", (req, res) => {
+  /* #swagger.tags = ['Python']
+        #swagger.description = 'Endpoint to upload image.'
+        #swagger.path = '/products/uploadImg'
+    */
   pythonController.uploaderPy(req, res);
 });
 
 
-PythonRouter.post("/uploadPy",jsonParser, (req, res) => {
+PythonRouter.post("/uploadPy", jsonParser, (req, res) => {
+  /* #swagger.tags = ['Python']
+       #swagger.description = 'Endpoint to upload python file.'
+       #swagger.path = '/products/uploadPy'
+       #swagger.parameters['file'] = {
+         in: 'body',
+         description: 'Python file',
+         required: true,
+         type: 'file'
+       } */
   pythonController.uploadPy(req, res);
 });
 
